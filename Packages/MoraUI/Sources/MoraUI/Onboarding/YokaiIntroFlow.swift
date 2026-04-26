de1369692d3db7aff5167b42f6e7de463bb6329e
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2026-04-26 00:18:45 -0700

    ui(onboarding): add YokaiIntroState 5-step machine

---
+++ Packages/MoraUI/Sources/MoraUI/Onboarding/YokaiIntroFlow.swift
@@ -0,0 +1,29 @@
+// Packages/MoraUI/Sources/MoraUI/Onboarding/YokaiIntroFlow.swift
+import Foundation
+import Observation
+
+@Observable
+@MainActor
+final class YokaiIntroState {
+    enum Step: Equatable, CaseIterable {
+        case concept, todayYokai, sessionShape, progress, finished
+    }
+
+    var step: Step = .concept
+
+    static let onboardedKey = "tech.reenable.Mora.yokaiIntroSeen"
+
+    func advance() {
+        switch step {
+        case .concept: step = .todayYokai
+        case .todayYokai: step = .sessionShape
+        case .sessionShape: step = .progress
+        case .progress: step = .finished
+        case .finished: break
+        }
+    }
+
+    func finalize(defaults: UserDefaults = .standard) {
+        defaults.set(true, forKey: Self.onboardedKey)
+    }
+}
